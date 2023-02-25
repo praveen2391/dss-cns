c70f416959a32dc66e13fbf3179365fc37890493
praveen <praveensrinu23@outlook.com>
2023-02-25 17:21:59 +0530

    az-kv-ap

--- az-keyvault-accpolicy/main.tf
+++ az-keyvault-accpolicy/main.tf
@@ -2,8 +2,8 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault_access_policy" "azkeyvaultap" {
   key_vault_id = azurerm_key_vault.azkeyvault.id
-  tenant_id    = var.tenant_id
-  object_id    = var.object_id
+  tenant_id    = var.tenant-id
+  object_id    = var.object-id
     
 
   key_permissions = [ "Get", ]
