21645e1e56a3398d802b754da6738ca20b62db7b
praveen <praveensrinu23@outlook.com>
2023-02-27 16:21:27 +0530

    az-kv

--- az-keyvault-accpolicy/variables.tf
+++ az-keyvault-accpolicy/variables.tf
@@ -7,5 +7,5 @@ variable "object-id" {
 }
 
 variable "key-vault-id" {
-  default = "/subscriptions/f09039d4-ce3c-47ae-8c5e-37d69e5c6fe6/resourceGroups/dss-rg/providers/Microsoft.KeyVault/vaults/azkeyvault123456789"
+#   default = "/subscriptions/f09039d4-ce3c-47ae-8c5e-37d69e5c6fe6/resourceGroups/dss-rg/providers/Microsoft.KeyVault/vaults/azkeyvault123456789"
 }
