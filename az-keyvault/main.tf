449b3aab446724ffdc0f67dad3c1a8417eb6b42f
praveen <praveensrinu23@outlook.com>
2023-02-27 11:48:27 +0530

    az-kv

--- az-keyvault/main.tf
+++ az-keyvault/main.tf
@@ -13,4 +13,18 @@ resource "azurerm_key_vault" "azkeyvault" {
 
 }  
 
+resource "azurerm_key_vault_access_policy" "azkeyvaultap" {
+  key_vault_id = azurerm_key_vault.azkeyvault.id
+  tenant_id    = var.tenant-id
+  object_id    = var.object-id
+    
+
+  key_permissions = [ "Get", ]
+    
+  secret_permissions = [ "Get", ]
+
+  storage_permissions = [ "Get", ]
+
+}
+
 
