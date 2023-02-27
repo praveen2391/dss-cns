da49f1b17738bc1ff2d6511cbb7de2cdcb4eb14c
praveen <praveensrinu23@outlook.com>
2023-02-27 18:36:18 +0530

    kv-ap

--- az-keyvault-accpolicy/main.tf
+++ az-keyvault-accpolicy/main.tf
@@ -6,10 +6,10 @@ resource "azurerm_key_vault_access_policy" "azkeyvaultap" {
   object_id    = var.object-id
     
 
-  key_permissions = [ "Get", "Set", "Create", "Delete", "Update", "Import" ]
+  key_permissions = [ "Get", "Create", "Delete", "Update", "Import", ]
     
-  secret_permissions = [ "Get", "Set", "Create", "Delete", "Update", "Import" ]
+  secret_permissions = [ "Get", "Set", "Delete", "List", ]
 
-  storage_permissions = [ "Get", "Set", "Create", "Delete", "Update", "Import" ]
+  storage_permissions = [ "Get", "Set", "Backup", "Delete", "Update", ]
 
 }
